6e9e6d59f56e384fcc80ee942862ea68f61b4c13
Zenonist <phonarnun@hotmail.com>
2025-04-15 18:09:32 +0700

    Add endpoint to return task information by UUID

--- src/main/java/com/task_tracker/tasks/services/TaskListService.java
+++ src/main/java/com/task_tracker/tasks/services/TaskListService.java
@@ -3,8 +3,11 @@ package com.task_tracker.tasks.services;
 import com.task_tracker.tasks.domain.entities.TaskList;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface TaskListService {
     List<TaskList> listTaskList();
     TaskList createTaskList(TaskList taskList);
+    Optional<TaskList> getTaskList(UUID id);
 }
