3e7c39e5b35e5110cf64b050261d361ad07d10ab
Zenonist <phonarnun@hotmail.com>
2025-04-16 02:50:22 +0700

    Add function to add new task in tasklist

---
+++ src/main/java/com/task_tracker/tasks/controllers/TaskController.java
@@ -0,0 +1,33 @@
+package com.task_tracker.tasks.controllers;
+
+import com.task_tracker.tasks.domain.dto.TaskDto;
+import com.task_tracker.tasks.mappers.TaskMapper;
+import com.task_tracker.tasks.services.TaskService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(path = "/task-lists/{task_list_id}/tasks")
+public class TaskController {
+
+    private final TaskService taskService;
+    private final TaskMapper taskMapper;
+
+    public TaskController(TaskService taskService, TaskMapper taskMapper) {
+        this.taskService = taskService;
+        this.taskMapper = taskMapper;
+    }
+
+    @GetMapping
+    public List<TaskDto> listTasks(@PathVariable("task_list_id") UUID taskListId){
+        return taskService.listTasks(taskListId)
+                .stream()
+                .map(taskMapper::toDto)
+                .toList();
+    }
+}
---
+++ src/main/java/com/task_tracker/tasks/services/TaskService.java
@@ -0,0 +1,10 @@
+package com.task_tracker.tasks.services;
+
+import com.task_tracker.tasks.domain.entities.Task;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface TaskService {
+    List<Task> listTasks(UUID taskListId);
+}
