6447f7e41a4e7bf8514fabf02a3bb6107413173a
Zenonist <phonarnun@hotmail.com>
2025-04-18 10:19:27 +0700

    feat: Integrate axios for API calls and update task list fetching logic

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,38 +1,51 @@
-import { useState } from 'react'
-import './App.css'
-import TaskList from './components/TaskList'
-import mockData from './mock-data/mock-data'
-
+import { useEffect, useState } from "react";
+import "./App.css";
+import TaskList from "./components/TaskList";
+import mockData from "./mock-data/mock-data";
+import TaskListProps from "./structure/TaskListProps";
+import { CirclePlus } from "lucide-react";
+import axios from "axios";
 function App() {
-  const [count, setCount] = useState(0)
+  const [taskList, setTaskList] = useState<TaskListProps[]>([]);
+
+  useEffect(() => {
+    // setTaskList(mockData);
+    getTaskList();
+    // It will execute only once when the website is loaded because there is no dependency
+  }, []);
+
+  const getTaskList = () => {
+    console.log(import.meta.env.VITE_API_URL)
+    axios
+    // ! Requires env name to have VITE_ prefix because of the vite requirement
+      .get(import.meta.env.VITE_API_URL + "/task-lists")
+      .then((response) => {
+        console.log(response.data);
+        setTaskList(response.data);
+      })
+      .catch((error) => console.error("Error fetching task lists:", error));
+  };
 
   return (
     <div className="min-h-screen bg-black text-white">
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <ul>
-          {mockData.map((task) => (
+      <h1 className="text-5xl text-center">Task Tracker</h1>
+      <div className="container mx-auto p-4">
+        {taskList.map((task) => (
+          <div key={task.id} className="mb-4">
             <TaskList
-              key={task.id}
               id={task.id}
               title={task.title}
               description={task.description}
               tasks={task.tasks}
             />
-          ))}
-        </ul>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+          </div>
+        ))}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="flex justify-center items-center text-2xl cursor-pointer">
+        <CirclePlus /> Add new Task List
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
